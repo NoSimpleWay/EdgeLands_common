75f19641f15ffeb7f37446e42f9e5d1be55fcb07
NoSimpleWay <36441257+NoSimpleWay@users.noreply.github.com>
2018-10-20 22:02:41 +0300

    source
    mech model and textures

--- core/src/com/midfag/game/GUI/buttons/ButtonEquip.java
+++ core/src/com/midfag/game/GUI/buttons/ButtonEquip.java
@@ -281,13 +281,16 @@ public class ButtonEquip extends Button {
 					
 					if ((inventory_id==-2)&&(GScreen.pl.inventory[99] instanceof Weapon))
 					{
-						GScreen.pl.armored[1].unequip();
+						if (GScreen.pl.armored[1]!=null)
+						{
+							GScreen.pl.armored[1].unequip();
+						}	
+							Object swap=(Weapon)GScreen.pl.armored[1];
+							GScreen.pl.armored[1]=(Weapon)GScreen.pl.inventory[99];
+							GScreen.pl.inventory[99]=swap;
+							
+							GScreen.pl.armored[1].equip();
 						
-						Object swap=(Weapon)GScreen.pl.armored[1];
-						GScreen.pl.armored[1]=(Weapon)GScreen.pl.inventory[99];
-						GScreen.pl.inventory[99]=swap;
-						
-						GScreen.pl.armored[1].equip();
 					}
 					
 					if ((inventory_id==-5)&&(GScreen.pl.inventory[99] instanceof Energoshield))
